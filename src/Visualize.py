6e493ed9aff2134f1b10a7d4c180f0a87d97a229
marble <msm.mirdamad@gmail.com>
2026-06-11 20:10:32 +0330

    add policy_map function for policy plotting

--- src/Visualize.py
+++ src/Visualize.py
@@ -9,6 +9,7 @@ def plot_all_visualizations(api, value_history, policy, delta_history, THETA):
 
     value_heatmap(api, value_history, rows, cols)
     convergence(delta_history, THETA)
+    policy_map(api, policy, rows, cols)
 
 def value_heatmap(api, value_history, rows, cols):
     for type, V in value_history.items():
@@ -34,25 +35,25 @@ def value_heatmap(api, value_history, rows, cols):
 
             for r in range(rows):
                 for c in range(cols):
-                    if api.api._env['startPos'] == {'row': r, 'col': c}:
+                    if api.server_api._env['startPos'] == {'row': r, 'col': c}:
                         ax.text(c, r, '🚁', ha='center', va='center', fontsize=12)
                     elif api.is_terminal((r, c, dmg, 0)):
                         ax.text(c, r, '🎯', ha='center', va='center', fontsize=12)
-                    elif api.api._is_obstacle(r, c):
+                    elif api.server_api._is_obstacle(r, c):
                         ax.text(c, r, '🧱', ha='center', va='center', fontsize=12)
-                    elif api.api._cell(r, c) == 'medkit':
+                    elif api.server_api._cell(r, c) == 'medkit':
                         ax.text(c, r, '💊', ha='center', va='center', fontsize=12)
-                    elif api.api._cell(r, c) in ('portal_a', 'portal_b'):
+                    elif api.server_api._cell(r, c) in ('portal_a', 'portal_b'):
                         ax.text(c, r, '🌀', ha='center', va='center', fontsize=12)
-                    elif api.api._cell(r, c) == 'storm':
-                        sev = api.api._storm_sev.get(f'{r},{c}', 1)
+                    elif api.server_api._cell(r, c) == 'storm':
+                        sev = api.server_api._storm_sev.get(f'{r},{c}', 1)
                         if sev == 1:
                             ax.text(c, r, '1', ha='center', va='center', fontsize=12)
                         elif sev == 2:
                             ax.text(c, r, '2', ha='center', va='center', fontsize=12)
                         elif sev == 3:
                             ax.text(c, r, '3', ha='center', va='center', fontsize=12)
-                    elif api.api.is_in_storm_zone((r, c, dmg)):
+                    elif api.server_api.is_in_storm_zone((r, c, dmg)):
                         ax.text(c+.5, r, '🌩️', ha='center', va='center', fontsize=12)
             
             plt.colorbar(im, ax=ax, shrink=0.6)
@@ -77,5 +78,66 @@ def convergence(delta_history, THETA):
         plt.legend(fontsize=11)
         
         output_path = os.path.join('visualize', f'convergence_{type}.png')
+        plt.savefig(output_path, dpi=300, bbox_inches='tight')
+        plt.close()
+
+def policy_map(api, policy, rows, cols):
+    for type, p in policy.items():
+        fig3, axes3 = plt.subplots(5, 1, figsize=(10, 22))
+        fig3.suptitle(f'{type.capitalize()} Optimal Policy Map for each Damage Level', 
+                      fontsize=14, fontweight='bold')
+        
+        action_makers = {
+            'N': '↑',
+            'S': '↓',
+            'E': '→',
+            'W': '←'
+        }
+        
+        for dmg in range(5):
+            ax = axes3[dmg]
+            ax.imshow(np.ones((rows, cols)), cmap='gray', vmin=0, vmax=1.1, origin='upper')
+            ax.set_title(f'Damage = {dmg}', fontsize=12)
+            ax.set_xticks(range(cols))
+            ax.set_yticks(range(rows))
+
+            ax.set_xticks(np.arange(-.5, cols, 1)),
+            ax.set_yticks(np.arange(-.5, rows, 1)),
+            ax.grid(which='minor', color='darkgray', linestyle='-', linewidth=1)
+            
+            for r in range(rows):
+                for c in range(cols):
+                    state = (r, c, dmg)
+                    
+                    plt.rcParams['font.family'] = ['Segoe UI Emoji']
+
+                    if api.server_api._env['startPos'] == {'row': r, 'col': c}:
+                        ax.add_patch(plt.Rectangle((c - 0.5, r - 0.5), 1, 1, color='limegreen', alpha=0.6))
+                    elif api.is_terminal((r, c, dmg, 0)):
+                        ax.add_patch(plt.Rectangle((c - 0.5, r - 0.5), 1, 1, color='limegreen', alpha=0.6))
+                        ax.text(c, r, '🎯', ha='center', va='center', fontsize=12)
+                        continue
+                    elif api.server_api._is_obstacle(r, c):
+                        ax.add_patch(plt.Rectangle((c - 0.5, r - 0.5), 1, 1, color='darkgray', alpha=0.6))
+                        ax.text(c, r, '🧱', ha='center', va='center', fontsize=12)
+                        continue
+                    elif api.server_api._cell(r, c) == 'medkit':
+                        ax.add_patch(plt.Rectangle((c - 0.5, r - 0.5), 1, 1, color='darkturquoise', alpha=0.6))
+                    elif api.server_api._cell(r, c) in ('portal_a', 'portal_b'):
+                        ax.add_patch(plt.Rectangle((c - 0.5, r - 0.5), 1, 1, color='mediumslateblue', alpha=0.6))
+                    elif api.server_api._cell(r, c) == 'storm':
+                        sev = api.server_api._storm_sev.get(f'{r},{c}', 1)
+                        ax.add_patch(plt.Rectangle((c - 0.5, r - 0.5), 1, 1, color='red', alpha=0.6))
+                    elif api.server_api.is_in_storm_zone((r, c, dmg)):
+                        ax.add_patch(plt.Rectangle((c - 0.5, r - 0.5), 1, 1, color='chocolate', alpha=0.6))
+                    
+                    act = p.get(state)
+                    if act in action_makers:
+                        ax.text(c, r, action_makers[act], ha='center', va='center', 
+                                color='royalblue', fontsize=18, fontweight='bold')
+        
+        output_path = os.path.join('visualize', f'policy_map_{type}.png')
+        plt.tight_layout(h_pad=2.0)
+        plt.subplots_adjust(top=0.95)
         plt.savefig(output_path, dpi=300, bbox_inches='tight')
         plt.close()
